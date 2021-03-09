51ac02d1107627948c933b95d161352f45c8ddde
Fadi Mashan <fadi.mashan@gmail.com>
2021-03-09 16:25:18 +0100

    Add project files.

---
+++ Garage G5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Garage_G5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
