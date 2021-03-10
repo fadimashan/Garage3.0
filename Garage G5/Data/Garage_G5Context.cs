79beb42237b31ab1f940ef9374ab3f50f1a67680
Fadi Mashan <fadi.mashan@gmail.com>
2021-03-10 13:01:18 +0100

    init the ParkedVehicleModel and Controller

---
+++ Garage G5/Data/Garage_G5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Garage_G5.Models;
+
+namespace Garage_G5.Data
+{
+    public class Garage_G5Context : DbContext
+    {
+        public Garage_G5Context (DbContextOptions<Garage_G5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage_G5.Models.ParkedVehicle> ParkedVehicle { get; set; }
+    }
+}
