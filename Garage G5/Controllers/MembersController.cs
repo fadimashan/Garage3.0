5599a1476996760d5bec6248d1493e0b1f1d54e5
MohamedHlabba <mohamed.hlabba@esprit.tn>
2021-03-25 17:10:40 +0100

    Add new CheckName AttributeValidation Add Date Of Birth AttributeValidation

--- Garage G5/Controllers/MembersController.cs
+++ Garage G5/Controllers/MembersController.cs
@@ -54,7 +54,7 @@ namespace Garage_G5.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Phone,Age,PersonalIdNumber,DateAdded,BonusAccountExpires")] Member member)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Phone,Age,PersonalIdNumber,DateAdded,BonusAccountExpires,DateOfBirth")] Member member)
         {
             if (ModelState.IsValid)
             {
--- Garage G5/Models/Member.cs
+++ Garage G5/Models/Member.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Garage_G5.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,10 +14,15 @@ namespace Garage_G5.Models
         public int Id { get; set; }
 
         [Display(Name = "First Name")]
+        [CheckName]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public MembershipType MembershipType { get; set; }
         public string Phone { get; set; }
+        [MinimumAge(18)]
+        [DisplayName("Date of Birth")]
+        [DataType(DataType.Date),DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}", ApplyFormatInEditMode =true)]
+        public DateTime DateOfBirth { get; set; }
         public int Age { get; set; }
         public string PersonalIdNumber { get; set; }
         public DateTime DateAdded { get ; set; }
