0b96355e837ede616a90cb58a5f550a3befc03d9
MohamedHlabba <mohamed.hlabba@esprit.tn>
2021-03-25 23:53:17 +0100

    Add CheckAgeAttribute Validation

--- Garage G5/Models/Member.cs
+++ Garage G5/Models/Member.cs
@@ -19,11 +19,13 @@ namespace Garage_G5.Models
         public string LastName { get; set; }
         public MembershipType MembershipType { get; set; }
         public string Phone { get; set; }
-        [MinimumAge(18)]
+        //[MinimumAge(18)]
+        [Min18Years]
         [DisplayName("Date of Birth")]
         [DataType(DataType.Date),DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}", ApplyFormatInEditMode =true)]
         public DateTime DateOfBirth { get; set; }
         public int Age { get; set; }
+       [SwedishPersonalNumber]
         public string PersonalIdNumber { get; set; }
         public DateTime DateAdded { get ; set; }
         public DateTime BonusAccountExpires { get; set; }
---
+++ Garage G5/Validation/SwedishPersonalNumberAttribute.cs
@@ -0,0 +1,46 @@
+﻿using Garage_G5.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_G5.Validation
+{
+    public class SwedishPersonalNumberAttribute : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            
+             if(value is string input && input.Length==8)
+             {
+                var owner = (Member)validationContext.ObjectInstance;
+               
+              var dateOfB = owner.DateOfBirth.ToString().Substring(0,4);
+                var person = owner.PersonalIdNumber.ToString();
+             
+
+                var fourDigits = input.Substring(4);
+                if (int.TryParse(fourDigits,out int res))
+                {
+              var personNumer    = string.Concat(dateOfB ,res);
+                    //var personNumer = dateOfB+res;
+                    var pesNum = owner.PersonalIdNumber;
+                    if (pesNum == personNumer)
+                    {
+                        return ValidationResult.Success;
+
+                    }
+                    else return new ValidationResult("The format is wrong");
+
+                }
+                return new ValidationResult("The format is wrong");
+
+
+            }
+            return new ValidationResult("The format is wrong");
+        }
+    }
+    
+}
