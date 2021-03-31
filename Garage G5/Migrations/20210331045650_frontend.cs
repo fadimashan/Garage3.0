0e8ee5c8fadaa1a91ff86ae83d8effc11ce18854
monotones1 <monotones@hotmail.com>
2021-03-31 07:01:23 +0200

    final fixes

---
+++ Garage G5/Migrations/20210331045650_frontend.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage_G5.Migrations
+{
+    public partial class frontend : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExtraHourlyRate",
+                table: "TypeOfVehicle");
+
+            migrationBuilder.DropColumn(
+                name: "ExtraRate",
+                table: "TypeOfVehicle");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExtraHourlyRate",
+                table: "TypeOfVehicle",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExtraRate",
+                table: "TypeOfVehicle",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
